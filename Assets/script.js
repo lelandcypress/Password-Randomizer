ecb2b578c69b56d1a935e28c1388b4bf041d0e7e
lelandcypress <80779467+lelandcypress@users.noreply.github.com>
2021-05-20 12:01:43 -0400

    Finally got the user input to function the way I wanted.

--- Assets/script.js
+++ Assets/script.js
@@ -4,37 +4,25 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
-function writePassword() {
-    
-      
-    
+function writePassword() {    
         
-        
-
-        
-        function generatePassword (){
-        //User functionality
-        var pLength = window.prompt("Please enter a number between 8 and 256 to select your password length");
-        if (pLength > 256 || pLength < 8){
-            window.alert("Please enter a number between 8 and 256")}
-
-        var wantNum = window.confirm ("Do you want numbers?")
-        var wantChar = window.confirm ("Do you want special characters?")
-        var wantUp = window.confirm ("Do you want uppercase letters?")
-            
-            
-        
-         //BUILD OUT THE ABILITY TO RANDOMLY SELECT UPPERCASE, LOWERCASE, SPECIAL CHARACTER, AND NUMBER 
-        //generate lowercase letter//
-        function randLetter() {
+        function generatePassword (){                            
+            //options arrays
             var alphabet =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+            var alphabetUpper =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+            var specChar =["!","@","#","$","%","^","&","*","(",")","<",">"];    
+            //Functions noted earlier in the code are pushed here to allow for a loop 
+            var pArray = [];
+            
+           
+            //Functions will randomly choose a letter, uppercase, number btw 0-9, and special characters when run 
+            //generate lowercase letter//
+            function randLetter() {
             return alphabet[Math.floor(Math.random()*alphabet.length)]
             };
             
-            
             //generate random uppercase letter//
             function randLetterUpper() {
-            var alphabetUpper =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
             return alphabetUpper[Math.floor(Math.random()*alphabetUpper.length)]
             };
                 
@@ -45,34 +33,63 @@ function writePassword() {
     
             //generate random special characters. Converted to string characters
             function randSpecChar(){
-            var specChar =["!","@","#","$","%","^","&","*","(",")","<",">"]
             return specChar[Math.floor(Math.random()*specChar.length)]
             };
             
-            //will call random function 
-            
-            
-
-            function buildingBlocks(){
-            var blocks= [randLetter(),randLetterUpper(), randNum(),randSpecChar()]
-            return blocks[Math.floor(Math.random() * blocks.length)]
+            //Confirm User Criteria
+            function userSelection(){ 
+            var wantLow = window.confirm("Do you want lowercase letters?")
+            if(wantLow === true){
+                pArray.push(randLetter())
+            }
+            //Confirm User Criteria 
+            var wantNum = window.confirm ("Do you want numbers?")
+            if (wantNum === true) {
+                pArray.push(randNum())
             };
-                   
-            var blocks=buildingBlocks
+            //Confirm User Criteria 
+            var wantChar = window.confirm ("Do you want special characters?")
+            if(wantChar === true) {
+                pArray.push(randSpecChar())
+            };
+            //Confirm User Criteria 
+            var wantUp = window.confirm ("Do you want uppercase letters?")
+            if(wantUp === true) {
+            pArray.push(randLetterUpper())}
+}
+            //Prompts user for password length, creates variable that can be used later in code
+            var pLength = window.prompt("Please enter a number between 8 and 256 to select your password length");
+            if (pLength < 257 && pLength > 7){
+                userSelection()
+            }else{
+            window.alert("Please enter a number between 8 and 256")}
+
+           
+
+
+
+
+                //pick it up here try console logging displayArray to see what pops up//
+            //for(var i = 0; i < pLength; i++){
+              //  finalPush()}
+
+                
+           
+            
+        
+        };     
+            
 
             
             
         //find a way to repeatedly call the function and push the results into a new array.//     
         //Build user input
         //conditions?      
-        }
-        
-    
-            
+                            
             var password = generatePassword();
             var passwordText = document.querySelector("#password");
             passwordText.value = password;
-        };    
+    };    
 
 
 
